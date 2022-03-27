27b9dc1060f2b76fb0618f9f17efe5ceec4cc4db
sugestiarinda <sugestiarinda@gmail.com>
2022-03-27 18:54:31 +0700

    tugas pertama

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Sugesti Arinda Kusumawardani.
+ Saya tingga di daerah Jakarta.
+ Olah raga yang saya sukai adalah
+ Bulutangkis.";
+ }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,31 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Sugesti </li>
+        <li>Nama Tengah : Arinda </li>
+        <li>Nama Belakang : Kusumawardani </li>
+    </ul>
+    
+    <h4>Nim</h4>
+    <ul type="disc">
+        <li>19200673</li>
+    </ul>
+    
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Cakung, jakarta timur, Dki Jakarta</li>
+    </ul>
+
+    <h4>Tempat, Tanggal Lahir</h4>
+    <ul type="none">
+        <li>Cilacap, 4 Oktober 2001</li>
+    </ul>
+    
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulu Tangkis</li>
+    </ul>
+</section>
